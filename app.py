a6314acefd4e466b9330cf598f6781329e59e337
vipas189 <rokx987@gmail.com>
2025-03-18 10:23:30 +0200

    Init

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from extensions import db, migrate
 from models import student
 from routes import student_routes
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db.init_app(app)
-migrate.init_app(app, db) 
+def main():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-student_routes.init_student_routes(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    student_routes.init_student_routes(app)
+    app.run(debug=True)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
--- routes/student_routes.py
+++ routes/student_routes.py
@@ -1,11 +1,18 @@
 from flask import render_template
 import services.students_actions as st_act
 
-def init_student_routes(app):
-    @app.route('/students')
-    def students():
-        return render_template('students.html', students=st_act.view_students())
 
-    @app.route('/create', methods=['GET', 'POST'])
+def init_student_routes(app):
+    @app.route("/students")
+    def students():
+        return render_template("students.html", students=st_act.view_students())
+
+    @app.route("/create", methods=["GET", "POST"])
     def create():
         return
+
+
+def init_home_route(app):
+    @app.route("/")
+    def home():
+        return render_template("index.html")
